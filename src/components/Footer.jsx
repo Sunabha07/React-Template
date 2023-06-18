96e9165dc84c3696600c2f02d31da1357b45422e
Sunabha07 <sunabhapanda@gmail.com>
2023-06-18 12:40:41 +0530

    Init Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1> 
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="" target={"blank"}>
+                    Linkedin
+                    </a>
+                <a href="https://www.instagram.com/_.sunabhaaaa._/" target={"blank"}>
+                    Instagram
+                    </a>
+                <a href="https://www.facebook.com/mamata.panda.5209" target={"blank"}>
+                    Facebook
+                    </a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
